fc73bca351162a2c2a6285476601f844ffdd9c5e
laughingbman <laughingbman@gmail.com>
2017-02-26 15:28:59 -0500

    Initial project testing
    Added unity chan and a free monster asset to a blank play field, worked
    on their corresponding triggers an anitmations.  Tested a load scene for
    combat once collision occurs.  Everything is currently client side only
    .

---
+++ ShadowMonsters/Assets/Scripts/ActorMovementController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActorMovementController : MonoBehaviour
+{
+    public float ScaleFactor = 0.25f;
+    private Vector3 _up;
+    private Vector3 _down;
+    private Vector3 _left;
+    private Vector3 _right;
+    private Animator _animator;
+
+    private GameObject _unityChan;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _up = new Vector3(0, 0, -1f*ScaleFactor);
+        _down = new Vector3(0, 0, 1f*ScaleFactor);
+        _left = new Vector3(1f * ScaleFactor, 0, 0);
+        _right = new Vector3(-1f * ScaleFactor, 0, 0);
+
+        _unityChan = transform.GetChild(0).gameObject;
+        _animator = _unityChan.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        _animator.SetFloat("Speed", 0.0f);
+
+
+        if (Input.GetKey(KeyCode.W))
+	    {
+            transform.position += _up;
+            //transform.Rotate(0.0f, 180.0f, 0.0f);
+            _unityChan.transform.forward = new Vector3(0,0,-1f);
+            _animator.SetFloat("Speed", 0.5f);
+        }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+            transform.position += _left;
+            //transform.Rotate(0.0f, 90.0f, 0.0f);
+            _unityChan.transform.forward = new Vector3(1f, 0, 0);
+            _animator.SetFloat("Speed", 0.5f);
+        }
+
+	    if (Input.GetKey(KeyCode.S))
+	    {
+            transform.position += _down;
+            //transform.Rotate(0.0f, 0.0f, 0.0f);
+            _unityChan.transform.forward = new Vector3(0, 0, 1f);
+            _animator.SetFloat("Speed", 0.5f);
+        }
+
+	    if (Input.GetKey(KeyCode.D))
+	    {
+            transform.position += _right;
+            //transform.Rotate(0.0f, 270.0f, 0.0f);
+            _unityChan.transform.forward = new Vector3(-1f, 0, 0);
+            _animator.SetFloat("Speed", 0.5f);
+        }
+	}
+
+    /// <summary>
+    /// this is exclusively used by rigid bodies
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter(Collision collision)
+    {
+    }
+
+    /// <summary>
+    /// This is a better solution for us because it allows us to handle movement without invoking the standard physics engine
+    /// for it to work make sure the game object has a collider and the trigger property of our actor is set to isKinematic = true
+    /// we wont actually care about mesh intersection with the shadows at this point, if we even keep that mechanic at all
+    /// </summary>
+    /// <param name="collider"></param>
+    void OnTriggerEnter(Collider collider)
+    {
+        var shadow = collider.gameObject.GetComponent<Shadow>();
+
+        if (shadow == null)
+            return;
+
+        
+        //we will eventually need to pass data through this more research on that later i suppose
+        //this is also currently nasty it just flash switches the entire view lol 
+        SceneManager.LoadScene("CombatScene");
+    }
+}
---
+++ ShadowMonsters/Assets/Scripts/Shadow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    public int GhostType;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}

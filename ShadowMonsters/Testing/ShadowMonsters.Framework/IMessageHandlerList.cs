16c2ca74eb5a5a676b7ff3b7a7550e9b74a32c0d
laughingbman <laughingbman@gmail.com>
2017-04-29 06:32:18 -0400

    Re removing as many bin files as a I can

--- ShadowMonsters/Testing/ShadowMonsters.Framework/IMessageHandlerList.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShadowMonsters.Framework
-{
-    public interface IMessageHandlerList<T>
-    {
-        bool RegisterHandler(IMessageHandler<T> handler);
-        bool HandleMessage(IMessage message, T peer);
-    }
-}
